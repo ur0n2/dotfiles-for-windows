63e3d83ad39fbf2ed5f4ae790f2a3a3c77545980
ur0n2 <ur0n2@ur0n2.com>
2017-04-06 15:38:28 +0900

    압축파일 확장자 연결프로그램 7z으로 등록 close #24

--- setup_ps.ps1
+++ setup_ps.ps1
@@ -10,12 +10,12 @@ function MAKE_DIR_FOR_DESKTOP{
 
 function ENV_VAR_REGISTER{
     Write-Output "[+] linked 환경변수(%path%) 등록" #issue19
-    $curr_path = @([Environment]::GetEnvironmentVariable("Path", "Machine")) -split ";" #$curr_path = @($env:Path) -split ";"
-    $curr_path
-    if ($curr_path -contains $env:ur0n2){
+    $add_path = write-debug $env:systemdrive"\linked;"$env:systemdrive"\linked\for_my\lnk;"$env:systemdrive"\linked\for_intranet\lnk"
+    $curr_path = @([Environment]::GetEnvironmentVariable("Path", "Machine")) -split ";" #$curr_path = @($env:Path) -split ";"    
+    if ($curr_path -contains $env:ur0n2 -and $env:ur0n2 -contains $add_path){
         Write-Output "`"ur0n2`" Environment Variable is already exist"
     }else{
-        $add_path = write-output $env:systemdrive"\linked;"$env:systemdrive"\linked\for_my\lnk;"$env:systemdrive"\linked\for_intranet\lnk"
+        
         [Environment]::SetEnvironmentVariable("ur0n2", $add_path, "Machine")
         [Environment]::GetEnvironmentVariable("ur0n2", "Machine")
         [Environment]::GetEnvironmentVariable("Path", "Machine")
@@ -52,6 +52,39 @@ function PICPICK_SETTING{
     start .\make_picpick_ini.bat
 }
 
+    Function COMPRESS_EXTENSION_LINK_TO_7Z_REGISTER{
+    $ext_path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\"
+    $compress_ext = @(".7z", ".XZ", ".BZIP2", ".GZIP", ".TAR", ".ZIP and WIM..AR", ".ARJ", ".CAB", ".CHM", ".CPIO", ".CramFS", ".DMG", ".EXT", ".FAT", ".GPT", ".HFS", ".IHEX", ".ISO", ".LZH", ".LZMA", ".MBR", ".MSI", ".NSIS", ".NTFS", ".QCOW2", ".RAR", ".RPM", ".SquashFS", ".UDF", ".UEFI", ".VDI", ".VHD", ".VMDK", ".WIM", ".XAR", ".Z")
+    $ext_key = @("OpenWithProgids", "OpenWithList", "UserChoice")
+
+    $i = 0
+    while($i -ne ($compress_ext.Length)){    
+        $ext_key_add_path = $ext_path + $compress_ext.GetValue($i)
+        New-Item -Path $ext_key_add_path
+        while($j -ne 3){
+            $ext_full_path = $ext_path + $compress_ext.GetValue($i) + "\" + $ext_key.GetValue($j) + "\"
+            #write-output $ext_full_path
+            New-Item -Path $ext_full_path
+        
+        
+            if($j -eq 0){
+                New-ItemProperty -Path $ext_full_path -Name "CompressedFolder" -PropertyType Binary
+            }
+            elseif($j -eq 1){
+                New-ItemProperty -Path $ext_full_path -Name "a" -PropertyType String -Value "7zFM.exe" | Out-NUL
+                New-ItemProperty -Path $ext_full_path -Name "MRUList" -PropertyType String -Value "a"
+            }
+            elseif($j -eq 2){
+                New-ItemProperty -Path $ext_full_path -Name "Progid" -PropertyType String -Value "Applications\7zFM.exe"
+            }
+            $j++
+        }
+        $j=0
+        $i++
+        #write-output ""
+    }
+}
+
 function HELP_MOD{
     Write-Output "[+] help 모듈"
     #help
@@ -60,6 +93,8 @@ function HELP_MOD{
 function SHELL_SENDTO_REGISTER{
     Write-Output "[+] shell:sendto 등록"
 }
+
+Set-ExecutionPolicy RemoteSigned -Force
 
 ENV_VAR_REGISTER
 <#
@@ -73,5 +108,8 @@ PICPICK_SETTING
 HELP_MOD
 SHELL_SENDTO_REGISTER
 #>
+Write-Output "[+] 압축파일 확장자 연결프로그램 7z으로 등록"
+COMPRESS_EXTENSION_LINK_TO_7Z_REGISTER 2> $NULL
 
-Read-Host 'Press Enter to continue…' | Out-Null
+
+#Read-Host 'Press Enter to continue…' | Out-Null

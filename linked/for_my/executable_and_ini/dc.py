0911bfe1995a3bcf096fb250d39e4d6e8731623b
ur0n2 <ur0n2@ur0n2.com>
2017-04-07 13:19:31 +0900

    tr.py dc.py dn.py no argument issue coded and url encode close #25 close #3

--- linked/for_my/executable_and_ini/dc.py
+++ linked/for_my/executable_and_ini/dc.py
@@ -1,12 +1,76 @@
-
 import subprocess
 import os
 import sys
+import ctypes
+from urllib import quote_plus
 
-base_url= "http://coredictionary.com/core/?w=" #Search query url
+OpenClipboard = ctypes.windll.user32.OpenClipboard
+EmptyClipboard = ctypes.windll.user32.EmptyClipboard
+GetClipboardData = ctypes.windll.user32.GetClipboardData
+SetClipboardData = ctypes.windll.user32.SetClipboardData
+CloseClipboard = ctypes.windll.user32.CloseClipboard
+CF_UNICODETEXT = 13
 
-value = sys.argv[1] #search value
-prgm_path = os.environ.get("PROGRAMFILES(X86)") #Program Files (x86) path
+GlobalAlloc = ctypes.windll.kernel32.GlobalAlloc
+GlobalLock = ctypes.windll.kernel32.GlobalLock
+GlobalUnlock = ctypes.windll.kernel32.GlobalUnlock
+GlobalSize = ctypes.windll.kernel32.GlobalSize
+GMEM_MOVEABLE = 0x0002
+GMEM_ZEROINIT = 0x0040
 
-query = prgm_path + "\Google\Chrome\Application\chrome.exe " + base_url + value #Make query
-subprocess.check_call(query, shell=False)
+unicode_type = type(u'')
+
+def get():
+    text = None
+    OpenClipboard(None)
+    handle = GetClipboardData(CF_UNICODETEXT)
+    pcontents = GlobalLock(handle)
+    size = GlobalSize(handle)
+    if pcontents and size:
+        raw_data = ctypes.create_string_buffer(size)
+        ctypes.memmove(raw_data, pcontents, size)
+        text = raw_data.raw.decode('utf-16le').rstrip(u'\0')
+    GlobalUnlock(handle)
+    CloseClipboard()
+    return text
+
+def put(s):
+    if not isinstance(s, unicode_type):
+        s = s.decode('mbcs')
+    data = s.encode('utf-16le')
+    OpenClipboard(None)
+    EmptyClipboard()
+    handle = GlobalAlloc(GMEM_MOVEABLE | GMEM_ZEROINIT, len(data) + 2)
+    pcontents = GlobalLock(handle)
+    ctypes.memmove(pcontents, data, len(data))
+    GlobalUnlock(handle)
+    SetClipboardData(CF_UNICODETEXT, handle)
+    CloseClipboard()
+
+paste = get
+copy = put
+
+prgm_path = ""
+if os.environ.get("PROGRAMFILES(X86)") is None: #this case is 32bit 
+    prgm_path = os.environ.get("PROGRAMFILES")
+else:
+    prgm_path = os.environ.get("PROGRAMFILES(X86)")
+#print prgm_path
+base_url= "http://coredictionary.com/core/?w=" #Core Dictionary search query url
+
+
+if len(sys.argv) is 1:
+    print "You don't type argument(s)"
+    value = quote_plus(str(get())) #quote is not encode to slash('/')
+    #print "testvalue: " +  value
+
+    query = prgm_path + "\Google\Chrome\Application\chrome.exe " + str(base_url) + value #Make query
+    print query
+    subprocess.check_call(query, shell=False)
+else:
+    value = quote_plus(" ".join(sys.argv[1:])) #[6:-6] #quote is not encode to slash('/')
+    #print "testvalue: " +  value
+
+    query = prgm_path + "\Google\Chrome\Application\chrome.exe " + str(base_url) + value #Make query
+    print query
+    subprocess.check_call(query, shell=False)

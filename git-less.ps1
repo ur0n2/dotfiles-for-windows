faaeebbc4bca949db4345ae927d081538446c43f
ur0n2 <ur0n2@ur0n2.com>
2017-05-10 13:06:29 +0900

    Update Installation Script with version float/int type bug fix

--- git-less.ps1
+++ git-less.ps1
@@ -1,6 +1,6 @@
 Set-Executionpolicy ByPass;
 cd $env:systemdrive\;
-if ( (([Environment]::OSVersion.Version | % {"{0}.{1}" -f $_.Major,$_.Minor}) -ge 6.1) -and ($PSVersionTable.PSVersion.Major -ge 4) -and ($PSVersionTable.CLRVersion.Major -ge 4) ){write-output "[+] Version Check PASS"}else{write-output "[+] Required to above PS 4.0, above Windows 6.1 version";start-sleep -s 3;exit;};
+if ( ([float]([Environment]::OSVersion.Version | % {"{0}.{1}" -f $_.Major,$_.Minor}) -ge 6.1) -and [int]($PSVersionTable.PSVersion.Major -ge 4) -and [int]($PSVersionTable.CLRVersion.Major -ge 4) ){write-output "[+] Version Check PASS"}else{write-output "[+] Required to above PS 4.0, above Windows 6.1 version";start-sleep -s 3;exit;};
 $uri=write-output https://github.com/ur0n2/dotfiles-for-windows/archive/master.zip;
 $outfile=write-output $env:systemdrive\dotfiles-for-windows-master.zip;
 $src=write-output $env:systemdrive\dotfiles-for-windows-master.zip;

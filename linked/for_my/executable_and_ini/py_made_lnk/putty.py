ebe401fb70e9c733c3bd21d50766778702bb4632
ur0n2 <ur0n2@ur0n2.com>
2017-05-23 08:57:38 +0900

    Add .gitignore

--- linked/for_my/executable_and_ini/py_made_lnk/putty.py
+++ linked/for_my/executable_and_ini/py_made_lnk/putty.py
@@ -8,12 +8,14 @@ def dir_recursion(prgm_path):
 			fname = os.path.split(file)
 			if fname[-1] == "putty.exe": 
 				print "get:" + fname[-1]
+				print "file:"+ file, path
 				print "got it!"
-	return fname
+	return fname, path
 
 
 print "[+] First execute to " + "'" + os.path.basename(os.path.realpath(__file__)) + "'"
 
+print os.path.basename(os.path.realpath(__file__)).split(".")[0]
 
 # case 1
 prgm_64path = os.environ.get("programfiles") # C:\Program Files (x86)
@@ -24,20 +26,40 @@ prgm_32path = os.environ.get("programW6432") # C:\Program Files
 #print prgm_path
 
 # if 64 bit and then run case1 and case2
-if os.environ.get("PROGRAMFILES(X86)") is None: # this case is 32bit
+# if 32bit and then just run case2.
+if prgm_64path is None: # this case is 32bit. programfiles(x86) path is not exist. that mean is x86 system.
 	print("this case is 32bit")
-	#print dir_recursion(prgm_32path)
+	
+	[execute, paths] = dir_recursion(prgm_32path)
+	if [execute, paths] is None:
+		print "what the " + os.path.basename(os.path.realpath(__file__)).split(".")[0] + " is not installed."
+	else:
+		print "wOw " + os.path.basename(os.path.realpath(__file__)).split(".")[0] + " is installed."
 else: # this case is 64bit
 	print("this case is 64bit")	
-	print dir_recursion(prgm_64path)
-	print dir_recursion(prgm_32path)
+	
+	[execute, paths] = dir_recursion(prgm_64path)
+	if [execute, paths] is None:
+		print "64bit path is None"
+	else:
+		[execute, paths] = dir_recursion(prgm_32path) 
+		if [execute, paths] is None:
+			print "what the " + os.path.basename(os.path.realpath(__file__)).split(".")[0] + " is not installed."
+		else:
+			print "wOw " + os.path.basename(os.path.realpath(__file__)).split(".")[0] + " is installed."
+	
 
-print "end"
-# if 32bit and then just run case2.
-
+# 
 
 
 # make symbolic link
 
 
 # execute count logging
+
+
+
+ㅁㄴ러ㅏㅣㅁ노림ㄴ라ㅣㅁㄴ라ㅣ
+설계가잘못됨 
+
+
